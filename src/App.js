2832616b4abf7b7bb296348361e185482a0c06b0
unknown <kachwalahasan14@gmail.com>
2024-08-28 17:21:34 +0530

    rechange1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import bg1 from './assest/react1.png';
+import Shabbirbhai from './component/header.js'
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-      <img src={bg1}></img>
-    </div>
+    <>
+    <Shabbirbhai/>
+    </>
   );
 }
 
